f11f3dbb37e43519791cbca9e422598c9938e4f3
B00165618 Artem Mochuliak <B00165618@mytudublin.ie>
2025-02-10 17:37:27 +0000

    changed printSteps() function in part B to take two arguments

---
+++ PartB.py
@@ -0,0 +1,9 @@
+#Artem Mochuliak
+#10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen with a function that takes 2 arguments witj for loop
+def printSteps(a,b):
+    for i in range(a,b+1):
+        print(i)
+
+    
+printSteps(1,9)
